241cc10c7dd79bc7e5b7518c3c54154529ea3744
hellodeloo <hello@hellodeloo.com>
2023-04-21 11:24:53 +0200

    fix: add show/hide input=password

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -184,12 +184,12 @@ $('[data-password=toggle]').on('click tap', function() {
   let thisSpan = $(this).children('span');
   let thisParent = $(this).closest('.input-group');
   let thisInput = $(thisParent).find('.form-control');
-  if (thisIcone.hasClass('ri-eye-off-line')) {
-    thisIcone.removeClass('ri-eye-off-line').addClass('ri-eye-line');
+  if (thisIcone.hasClass('ri-eye-line')) {
+    thisIcone.removeClass('ri-eye-line').addClass('ri-eye-off-line');
     thisSpan.html('Masquer');
     thisInput.attr('type', 'text');
   } else {
-    thisIcone.removeClass('ri-eye-line').addClass('ri-eye-off-line');
+    thisIcone.removeClass('ri-eye-off-line').addClass('ri-eye-line');
     thisSpan.html('Afficher');
     thisInput.attr('type', 'password');
   }
