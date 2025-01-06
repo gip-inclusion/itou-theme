03b4cde54b2671c286055c00ac463770c38d2a02
hellodeloo <hello@hellodeloo.com>
2025-01-06 17:29:53 +0100

    fix: .inputgroup and easyMDE focus styles

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,14 +1,13 @@
 // Import our custom CSS
-import '../stylesheets/app.scss?inline';
+import "../stylesheets/app.scss?inline";
 
 // Common vars
-const alertDismissibleOnceList = document.querySelectorAll('.alert-dismissible-once');
-const textareaExpandableList = document.querySelectorAll('[data-it-expandable=true]');
-const inputGroupList = document.querySelectorAll('.input-group');
-const inputPasswordList = document.querySelectorAll('[data-it-password=toggle]');
-const targetConseiList = document.querySelectorAll('[data-it-target-conseil]');
-const actionPrintList = document.querySelectorAll('[data-it-action=print]');
-const breakpointXL = getComputedStyle(document.documentElement).getPropertyValue('--bs-breakpoint-xl');
+const alertDismissibleOnceList = document.querySelectorAll(".alert-dismissible-once");
+const textareaExpandableList = document.querySelectorAll("[data-it-expandable=true]");
+const inputPasswordList = document.querySelectorAll("[data-it-password=toggle]");
+const targetConseiList = document.querySelectorAll("[data-it-target-conseil]");
+const actionPrintList = document.querySelectorAll("[data-it-action=print]");
+const breakpointXL = getComputedStyle(document.documentElement).getPropertyValue("--bs-breakpoint-xl");
 
 // Init
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
@@ -18,7 +17,7 @@ const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]
 [...popoverTriggerList].map((popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl));
 
 // Load
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   // console.log('window loaded');
   postHeaderNavDisplay();
   inputGroupCopyToClipboard();
@@ -26,50 +25,49 @@ window.addEventListener('load', () => {
 });
 
 // Resize
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   // console.log('window resized');
   postHeaderNavDisplay();
 });
 
 // Scroll
 // window.addEventListener('scroll', () => {
-  // console.log('window scrolled');
+// console.log('window scrolled');
 // });
 
 // Htmx Load
-document.addEventListener('htmx:load', () => {
+document.addEventListener("htmx:load", () => {
   // console.log('document htmx loaded');
   inputGroupCopyToClipboard();
   buttonCopyToClipboard();
 });
 
-
 // Post header show/hide on scroll up/down
 function postHeaderNavDisplay() {
-  const postHeaderNav = document.querySelector('.s-postheader');
+  const postHeaderNav = document.querySelector(".s-postheader");
 
   if (postHeaderNav != null) {
     let thisNavTopOffset = postHeaderNav.getBoundingClientRect().top;
     let lastScrollTop = 0;
 
-    window.addEventListener('scroll', () => {
+    window.addEventListener("scroll", () => {
       let windowScrollTop = window.scrollY;
 
       if (windowScrollTop >= thisNavTopOffset) {
-        if (window.matchMedia('(min-width: ' + breakpointXL + ')').matches) {
-          document.querySelector('main').style.paddingTop = '59px';
+        if (window.matchMedia("(min-width: " + breakpointXL + ")").matches) {
+          document.querySelector("main").style.paddingTop = "59px";
         }
         if (lastScrollTop > windowScrollTop) {
-          postHeaderNav.classList.remove('it-scrolldown');
-          postHeaderNav.classList.add('it-scrollup');
+          postHeaderNav.classList.remove("it-scrolldown");
+          postHeaderNav.classList.add("it-scrollup");
         } else {
-          postHeaderNav.classList.remove('it-scrollup');
-          postHeaderNav.classList.add('it-scrolldown');
+          postHeaderNav.classList.remove("it-scrollup");
+          postHeaderNav.classList.add("it-scrolldown");
         }
       } else {
-        postHeaderNav.classList.remove('it-scrollup', 'it-scrolldown');
-        if (window.matchMedia('(min-width: ' + breakpointXL + ')').matches) {
-          document.querySelector('main').style.paddingTop = '0';
+        postHeaderNav.classList.remove("it-scrollup", "it-scrolldown");
+        if (window.matchMedia("(min-width: " + breakpointXL + ")").matches) {
+          document.querySelector("main").style.paddingTop = "0";
         }
       }
       lastScrollTop = windowScrollTop;
@@ -79,14 +77,14 @@ function postHeaderNavDisplay() {
 
 // InputGroup copy to clipboard
 function inputGroupCopyToClipboard() {
-  const clipboardCopyList = document.querySelectorAll('[data-it-clipboard=copy]');
+  const clipboardCopyList = document.querySelectorAll("[data-it-clipboard=copy]");
   for (let i = 0, ii = clipboardCopyList.length; i < ii; i += 1) {
     const thisClipboardCopy = clipboardCopyList[i];
-    const thisInputGroupClipboardCopy = thisClipboardCopy.closest('.input-group');
-    const thisFormControlClipboardCopy = thisInputGroupClipboardCopy.querySelector('.form-control').value;
+    const thisInputGroupClipboardCopy = thisClipboardCopy.closest(".input-group");
+    const thisFormControlClipboardCopy = thisInputGroupClipboardCopy.querySelector(".form-control").value;
     const thisTooltip = bootstrap.Tooltip.getOrCreateInstance(thisClipboardCopy);
 
-    thisClipboardCopy.addEventListener('click', function () {
+    thisClipboardCopy.addEventListener("click", function () {
       navigator.clipboard
         .writeText(thisFormControlClipboardCopy)
         .then(() => {
@@ -98,7 +96,7 @@ function inputGroupCopyToClipboard() {
       thisTooltip.show();
     });
 
-    thisClipboardCopy.addEventListener('blur', function () {
+    thisClipboardCopy.addEventListener("blur", function () {
       thisTooltip.hide();
     });
   }
@@ -106,13 +104,13 @@ function inputGroupCopyToClipboard() {
 
 // Button copy to clipboard
 function buttonCopyToClipboard() {
-  const clipboardButtonCopyList = document.querySelectorAll('[data-it-clipboard-button=copy]');
+  const clipboardButtonCopyList = document.querySelectorAll("[data-it-clipboard-button=copy]");
   for (let i = 0, ii = clipboardButtonCopyList.length; i < ii; i += 1) {
     const thisClipboardButtonCopy = clipboardButtonCopyList[i];
     const thisClipboardButtonCopyValue = thisClipboardButtonCopy.dataset.itCopyToClipboard;
     const thisTooltip = bootstrap.Tooltip.getOrCreateInstance(thisClipboardButtonCopy);
 
-    thisClipboardButtonCopy.addEventListener('click', function () {
+    thisClipboardButtonCopy.addEventListener("click", function () {
       navigator.clipboard
         .writeText(thisClipboardButtonCopyValue)
         .then(() => {
@@ -124,7 +122,7 @@ function buttonCopyToClipboard() {
       thisTooltip.show();
     });
 
-    thisClipboardButtonCopy.addEventListener('blur', function () {
+    thisClipboardButtonCopy.addEventListener("blur", function () {
       thisTooltip.hide();
     });
   }
@@ -133,22 +131,22 @@ function buttonCopyToClipboard() {
 // InputGroup toggle input text/password
 for (let i = 0, ii = inputPasswordList.length; i < ii; i += 1) {
   const thisInputPassword = inputPasswordList[i];
-  const thisInputGroupInputPassword = thisInputPassword.closest('.input-group');
-  const thisFormControlInputPassword = thisInputGroupInputPassword.querySelector('.form-control');
-  const thisIconeInputPassword = thisInputPassword.querySelector('i');
-  const thisSpanInputPassword = thisInputPassword.querySelector('span');
+  const thisInputGroupInputPassword = thisInputPassword.closest(".input-group");
+  const thisFormControlInputPassword = thisInputGroupInputPassword.querySelector(".form-control");
+  const thisIconeInputPassword = thisInputPassword.querySelector("i");
+  const thisSpanInputPassword = thisInputPassword.querySelector("span");
 
-  thisInputPassword.addEventListener('click', function () {
-    if (thisIconeInputPassword.classList.contains('ri-eye-line')) {
-      thisIconeInputPassword.classList.remove('ri-eye-line');
-      thisIconeInputPassword.classList.add('ri-eye-off-line');
-      thisFormControlInputPassword.setAttribute('type', 'text');
-      thisSpanInputPassword.innerHTML = 'Masquer';
+  thisInputPassword.addEventListener("click", function () {
+    if (thisIconeInputPassword.classList.contains("ri-eye-line")) {
+      thisIconeInputPassword.classList.remove("ri-eye-line");
+      thisIconeInputPassword.classList.add("ri-eye-off-line");
+      thisFormControlInputPassword.setAttribute("type", "text");
+      thisSpanInputPassword.innerHTML = "Masquer";
     } else {
-      thisIconeInputPassword.classList.remove('ri-eye-off-line');
-      thisIconeInputPassword.classList.add('ri-eye-line');
-      thisFormControlInputPassword.setAttribute('type', 'password');
-      thisSpanInputPassword.innerHTML = 'Afficher';
+      thisIconeInputPassword.classList.remove("ri-eye-off-line");
+      thisIconeInputPassword.classList.add("ri-eye-line");
+      thisFormControlInputPassword.setAttribute("type", "password");
+      thisSpanInputPassword.innerHTML = "Afficher";
     }
   });
 }
@@ -156,7 +154,7 @@ for (let i = 0, ii = inputPasswordList.length; i < ii; i += 1) {
 // Data action to print page
 for (let i = 0, ii = actionPrintList.length; i < ii; i += 1) {
   const thisActionPrint = actionPrintList[i];
-  thisActionPrint.addEventListener('click', function print() {
+  thisActionPrint.addEventListener("click", function print() {
     window.print();
   });
 }
@@ -164,18 +162,18 @@ for (let i = 0, ii = actionPrintList.length; i < ii; i += 1) {
 // Show/Hide Alert dismissible only once
 for (let i = 0, ii = alertDismissibleOnceList.length; i < ii; i += 1) {
   const thisAlert = alertDismissibleOnceList[i];
-  const thisID = thisAlert.getAttribute('id');
-  const thisButtonClose = thisAlert.querySelector('.btn-close');
+  const thisID = thisAlert.getAttribute("id");
+  const thisButtonClose = thisAlert.querySelector(".btn-close");
 
   const seenAlert = localStorage.getItem(thisID);
 
   if (seenAlert === null) {
-    thisAlert.classList.remove('d-none');
+    thisAlert.classList.remove("d-none");
   }
 
-  thisButtonClose.addEventListener('click', function () {
-    localStorage.setItem(thisID, 'seen');
-    thisAlert.classList.add('d-none');
+  thisButtonClose.addEventListener("click", function () {
+    localStorage.setItem(thisID, "seen");
+    thisAlert.classList.add("d-none");
   });
 }
 
@@ -184,39 +182,26 @@ for (let i = 0, ii = textareaExpandableList.length; i < ii; i += 1) {
   const thisTexterea = textareaExpandableList[i];
 
   const expandTextarea = function () {
-    thisTexterea.style.removeProperty('height');
-    thisTexterea.style.height = this.scrollHeight + 2 + 'px';
+    thisTexterea.style.removeProperty("height");
+    thisTexterea.style.height = this.scrollHeight + 2 + "px";
   };
 
-  thisTexterea.addEventListener('keydown', expandTextarea, false);
-  thisTexterea.addEventListener('mousedown', expandTextarea, false);
-}
-
-// Add global focus on input-group
-for (let i = 0, ii = inputGroupList.length; i < ii; i += 1) {
-  const thisInputGroup = inputGroupList[i];
-  const thisFormControl = thisInputGroup.querySelector('.form-control, .form-select');
-
-  const toggleFocus = function () {
-    thisInputGroup.classList.toggle('has-focus');
-  };
-
-  thisFormControl.addEventListener('focus', toggleFocus, false);
-  thisFormControl.addEventListener('blur', toggleFocus, false);
+  thisTexterea.addEventListener("keydown", expandTextarea, false);
+  thisTexterea.addEventListener("mousedown", expandTextarea, false);
 }
 
 // Show data-it-target-conseil
 for (let i = 0, ii = targetConseiList.length; i < ii; i += 1) {
   const thisTargetConseil = targetConseiList[i];
-  const thisTargetConseilID = thisTargetConseil.getAttribute('data-it-target-conseil');
+  const thisTargetConseilID = thisTargetConseil.getAttribute("data-it-target-conseil");
   const thisTargetID = document.querySelector(thisTargetConseilID);
 
-  thisTargetConseil.addEventListener('focus', function (e) {
+  thisTargetConseil.addEventListener("focus", function (e) {
     e.preventDefault();
-    thisTargetID.classList.remove('is-openable');
+    thisTargetID.classList.remove("is-openable");
   });
-  thisTargetConseil.addEventListener('blur', function (e) {
+  thisTargetConseil.addEventListener("blur", function (e) {
     e.preventDefault();
-    thisTargetID.classList.add('is-openable');
+    thisTargetID.classList.add("is-openable");
   });
 }
