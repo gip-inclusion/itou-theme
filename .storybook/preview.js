e91d49cab119e9f02a1b55b7f1dea20ecd77eff1
hellodeloo <hello@hellodeloo.com>
2026-02-21 11:06:32 +0100

    fix: standardize quotation marks in preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
-import '../dist/stylesheets/app.css';
-import '../dist/javascripts/app.js';
-import itouTheme from './itouTheme';
+import "../dist/stylesheets/app.css";
+import "../dist/javascripts/app.js";
+import itouTheme from "./itouTheme";
 
 /** @type { import('@storybook/html-vite').Preview } */
 const preview = {
@@ -9,10 +9,10 @@ const preview = {
       theme: itouTheme,
       codePanel: true,
       source: {
-        type: 'dynamic',
+        type: "dynamic",
         transform: (src, context) => {
           const rendered = context?.originalStoryFn?.(context.args, context);
-          return typeof rendered === 'string' ? rendered.trim() : src;
+          return typeof rendered === "string" ? rendered.trim() : src;
         },
       },
     },
@@ -22,7 +22,9 @@ const preview = {
         date: /Date$/i,
       },
     },
-
+    // viewport: {
+    //   defaultViewport: "responsive",
+    // },
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
